e51af3af62de1f1a844d595216a0ae3a42e82961
Katarzyna Kamieniecka <kamienieckakat@gmail.com>
2022-07-25 22:47:05 +0200

    arrows scrolling, gallery for creator

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const downloadBtn = document.getElementById("download-btn");
+// BUTTONS - STYLE AND FUNCTIONALITY
 
+const downloadBtn = document.getElementById("download-btn");
 const circleBtns = document.querySelectorAll("div.circle-btns > button");
 const rectBtns = document.querySelectorAll("div.rect-btns > button");
 const allMenuBtns = Array.prototype.concat.call(circleBtns, rectBtns);
@@ -33,3 +34,28 @@ function setInstruction() {
   instruction.innerHTML = `<span id="new-instruction">${chosenCategory}</span>`;
   instruction.classList.add("animation-on-showing");
 }
+
+// ARROWS - FUNCTIONALITY
+
+const imgContainer = document.querySelector(".carousel-choosing");
+
+function scrollLeft() {
+  imgContainer.scrollBy(-100, 0);
+}
+
+function scrollRight() {
+  imgContainer.scrollBy(100, 0);
+  //checking if it's the end of gallery
+  if (
+    imgContainer.offsetWidth + imgContainer.scrollLeft >=
+    imgContainer.scrollWidth
+  ) {
+    imgContainer.scrollBy(-imgContainer.scrollWidth, 0);
+  }
+}
+
+let Arrowleft = document.getElementById("arrow-l");
+let ArrowRight = document.getElementById("arrow-r");
+
+Arrowleft.addEventListener("click", scrollLeft);
+ArrowRight.addEventListener("click", scrollRight);
