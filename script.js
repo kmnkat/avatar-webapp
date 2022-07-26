8b5fc0a0b1d2a891528fe271b3fc5b2f7f2e103c
Katarzyna Kamieniecka <kamienieckakat@gmail.com>
2022-07-26 22:08:30 +0200

    other js, simple data fetching

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+$(document).ready(function () {
+  alert("jQuery Works");
+});
+
 // BUTTONS - STYLE AND FUNCTIONALITY
 
 const downloadBtn = document.getElementById("download-btn");
@@ -5,7 +9,7 @@ const circleBtns = document.querySelectorAll("div.circle-btns > button");
 const rectBtns = document.querySelectorAll("div.rect-btns > button");
 const allMenuBtns = Array.prototype.concat.call(circleBtns, rectBtns);
 
-let chosenCategory = "style";
+let chosenCategory = "hair";
 
 const instruction = document.getElementById("new-instruction");
 
@@ -26,6 +30,9 @@ allMenuBtns.forEach(function (buttonList) {
         chosenCategory = e.target.id;
       }
       setInstruction();
+      imgContainer.innerHTML = "";
+      renderNewElements();
+      console.log();
     });
   });
 });
@@ -59,3 +66,60 @@ let ArrowRight = document.getElementById("arrow-r");
 
 Arrowleft.addEventListener("click", scrollLeft);
 ArrowRight.addEventListener("click", scrollRight);
+
+// SHOW IMAGES IN THE GALLERY
+
+document.addEventListener("DOMContentLoaded", renderNewElements);
+
+function renderNewElements() {
+  const req = new XMLHttpRequest();
+  req.open("GET", dataAboutImagesUrl, true);
+  req.send();
+  req.onload = function () {
+    const json = JSON.parse(req.responseText).record;
+
+    json[chosenCategory].forEach(function (element) {
+      let url = element.url;
+      let alt = element.alt;
+      let div = document.createElement("div");
+      div.classList.add("img-to-choose");
+      div.innerHTML = `<img src=${url} alt=${alt}/>`;
+      imgContainer.appendChild(div);
+    });
+    const imagesDisplayed = document.querySelectorAll(".img-to-choose");
+    imagesDisplayed.forEach(function (image) {
+      image.addEventListener("click", function (e) {
+        e.preventDefault();
+        let previousChosen = document.querySelector(".chosen-carousel-item");
+
+        if (previousChosen) {
+          previousChosen.classList.remove("chosen-carousel-item");
+        }
+
+        e.target.parentElement.classList.add("chosen-carousel-item");
+        console.log(e.target.alt);
+      });
+    });
+  };
+}
+
+// DOWNLOAD IMAGE
+
+const imageToDownload = document.getElementById("slogan");
+
+const domtoimg = document.createElement("script");
+domtoimg.type = "text/javascript";
+domtoimg.src =
+  "https://cdnjs.cloudflare.com/ajax/libs/dom-to-image/2.6.0/dom-to-image.js";
+document.head.appendChild(domtoimg);
+
+domtoimage
+  .toPng(imageToDownload)
+  .then(function (dataUrl) {
+    var img = new Image();
+    img.src = dataUrl;
+    document.body.appendChild(img);
+  })
+  .catch(function (error) {
+    console.error("oops, something went wrong!", error);
+  });
