0036cf090b37a4b9c26ccdea5a777a42ccc76b4b
Katarzyna Kamieniecka <kamienieckakat@gmail.com>
2022-07-27 23:27:46 +0200

    local storage - all basic func.

--- public/js/script.js
+++ public/js/script.js
@@ -3,40 +3,35 @@ let chosenCategory = "hair";
 // FETCHING DATA - SETTING INITIAL VALUES FOR PORTRAIT ELEMENTS
 function initialData() {
   console.log("initialization");
-  $.getJSON("./data/chosenElements.json", function (chosenItems) {
-    if (!localStorage.getItem("userItems")) {
-      localStorage.setItem("userItems", JSON.stringify(chosenItems));
-      console.log("initial set");
+  //setting initial data for the first use
+  creatingFirstLocalStorage();
+
+  for (const key in localStorage) {
+    //setting portraits elements (based on local storage)
+    if (localStorage.hasOwnProperty(key)) {
+      creatingImgByGivenAlt(localStorage.getItem(key), key);
     }
-
-    console.log(chosenItems);
-    $.each(chosenItems, function (key, val) {
-      //setting initial chosen data
-      if (key === chosenCategory) {
-        let image = document.querySelector(`img[alt="${val}"]`);
-        image.parentElement.classList.add("chosen-carousel-item");
+  }
+}
+//function for initial data for the first use from json file
+function creatingFirstLocalStorage() {
+  $.getJSON("./data/chosenElements.json", function (chosenItems) {
+    $.each(chosenItems, function (key, value) {
+      if (!window.localStorage.getItem(key)) {
+        window.localStorage.setItem(key, value);
       }
-
-      $.getJSON("./data/creatorElements.json", function (dataItem) {
-        $.each(dataItem[key], function (value) {
-          if (dataItem[key][value].alt === val) {
-            //console.log("TRY TO SET " + val);
-            creatingImgByGivenAlt(val, key);
-            //console.log(key + " created");
-          }
-        });
-      });
     });
   });
 }
 
+//function to create element based on limited data from the local storage or json file
 function creatingImgByGivenAlt(altVal, elementCategory) {
   $.getJSON("./data/creatorElements.json", function (item) {
-    //console.log(item[elementCategory][0].url);
     $.each(item[elementCategory], function (data, values) {
       if (values.alt === altVal) {
         let copyOfImg = document.createElement("img");
         copyOfImg.src = values.url;
+        copyOfImg.alt = values.alt;
         settingNewCreatorEl(copyOfImg, elementCategory, altVal);
       }
     });
@@ -90,6 +85,83 @@ function setInstruction() {
   instruction.classList.add("animation-on-showing");
 }
 
+// SHOW IMAGES IN THE GALLERY
+// render images for for the page load
+document.addEventListener("DOMContentLoaded", renderNewElements);
+
+function renderNewElements() {
+  // loading data from json file to the carousel gallery
+  $.getJSON("./data/creatorElements.json", function (data) {
+    $.each(data, function (key, val) {
+      if (key === chosenCategory) {
+        data[key].forEach(function (singleEl) {
+          let url = singleEl.url;
+          let alt = singleEl.alt;
+          // creating container div
+          let container = document.createElement("div");
+          container.classList.add("img-to-choose");
+          container.classList.add(`${key}-miniature`);
+          // creating img
+          let img = document.createElement("img");
+          img.src = `${url}`;
+          img.alt = `${alt}`;
+          //highlight if it's an item from local storage
+          if (img.alt === localStorage.getItem(key)) {
+            container.classList.add("chosen-carousel-item");
+          }
+          // creating copy to set as a portrait element
+          let copy = document.createElement("img");
+          copy.src = `${url}`;
+          container.appendChild(img);
+          // add event listener to each image div (change portrait element on click)
+          container.addEventListener("click", function (e) {
+            chosenItem(e.target);
+            settingNewCreatorEl(copy, chosenCategory, img.alt);
+          });
+
+          imgContainer.appendChild(container);
+        });
+      }
+    });
+  });
+
+  initialData();
+}
+
+//set the background to the chosen element (carousel gallery)
+function chosenItem(element) {
+  let previousChosen = document.querySelector(".chosen-carousel-item");
+  if (previousChosen) {
+    previousChosen.classList.remove("chosen-carousel-item");
+  }
+  element.classList.add("chosen-carousel-item");
+}
+
+//set the chosen element to portrait area
+function settingNewCreatorEl(image, imageCategory, altText) {
+  let choosingId = `${imageCategory}-creator`;
+  let choosingArea = document.getElementById(choosingId);
+  choosingArea.innerHTML = "";
+  choosingArea.appendChild(image);
+  for (let key in localStorage) {
+    if (key == imageCategory) {
+      window.localStorage.setItem(imageCategory, altText);
+    }
+  }
+}
+
+// DOWNLOAD IMAGE
+
+const imageToDownload = document.getElementById("user-image");
+const downloadButton = document.getElementById("download-btn");
+
+downloadButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  domtoimage.toBlob(imageToDownload).then(function (blob) {
+    window.saveAs(blob, "my-avatar.png");
+  });
+});
+
 // ARROWS - FUNCTIONALITY
 
 const imgContainer = document.querySelector(".carousel-choosing");
@@ -114,78 +186,3 @@ let ArrowRight = document.getElementById("arrow-r");
 
 Arrowleft.addEventListener("click", scrollLeft);
 ArrowRight.addEventListener("click", scrollRight);
-
-// SHOW IMAGES IN THE GALLERY
-// render images for for the first page load
-document.addEventListener("DOMContentLoaded", renderNewElements);
-
-function renderNewElements() {
-  // loading data from json file to the gallery
-  $.getJSON("./data/creatorElements.json", function (data) {
-    $.each(data, function (key, val) {
-      if (key === chosenCategory) {
-        data[key].forEach(function (singleEl) {
-          let url = singleEl.url;
-          let alt = singleEl.alt;
-          // creating container div
-          let container = document.createElement("div");
-          container.classList.add("img-to-choose");
-          container.id = `${alt}`;
-          // creating img
-          let img = document.createElement("img");
-          img.src = `${url}`;
-          img.alt = `${alt}`;
-          let copy = document.createElement("img");
-          copy.src = `${url}`;
-          container.appendChild(img);
-          container.addEventListener("click", function (e) {
-            chosenItem(e.target);
-            console.log("I set by clicking: " + e.target.id);
-            //console.log(copy);
-            settingNewCreatorEl(copy, chosenCategory, img.alt);
-          });
-
-          imgContainer.appendChild(container);
-        });
-      }
-    });
-  });
-  initialData();
-}
-
-//set the background to chosen elements
-function chosenItem(element) {
-  let previousChosen = document.querySelector(".chosen-carousel-item");
-  if (previousChosen) {
-    previousChosen.classList.remove("chosen-carousel-item");
-  }
-  element.classList.add("chosen-carousel-item");
-}
-
-//set the chosen element to portrait area
-function settingNewCreatorEl(image, imageCategory, altText) {
-  let choosingId = `${imageCategory}-creator`;
-  let choosingArea = document.getElementById(choosingId);
-  choosingArea.innerHTML = "";
-  choosingArea.appendChild(image);
-  $.getJSON("./data/chosenElements.json", function (itemToChangeData) {
-    if (altText != itemToChangeData[imageCategory]) {
-      itemToChangeData[imageCategory] = altText;
-      console.log(
-        "JSON new " + imageCategory + " is " + itemToChangeData[imageCategory]
-      );
-    }
-  });
-}
-
-// DOWNLOAD IMAGE
-
-const imageToDownload = document.getElementById("user-image");
-const downloadButton = document.getElementById("download-btn");
-
-downloadButton.addEventListener("click", function (e) {
-  e.preventDefault();
-  domtoimage.toBlob(imageToDownload).then(function (blob) {
-    window.saveAs(blob, "my-avatar.png");
-  });
-});
